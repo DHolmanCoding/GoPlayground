1e26a2527a5fcbed653dfefe65794d16de55442b
Douglas Holman <DouglasHolman9@gmail.com>
2020-07-05 15:32:04 -0700

    fix: updating pathing on service code

--- generate_protobuf_code.sh
+++ generate_protobuf_code.sh
@@ -1,4 +1,4 @@
 if [ "$PWD" -ef "/home/douglas/go/src/GoPlayground" ];
   then
-    protoc -I . --go_out=plugins=grpc:. GoProtoServiceConsignment/proto/consignment/consignment.proto
+    protoc -I . --go_out=plugins=grpc:. go-playground-service-consignment/proto/consignment/consignment.proto
 fi
